80936c8001e1dd64a1036a54e7fefd98f934c79c
Ivan Arcuschin <iarcuschin@gmail.com>
2026-01-26 19:15:06 -0300

    Run AIME and coding benchmarks only on inputs where thinking model finishes

--- hybrid/run_qwen_32b_on_deepseek.sh
+++ hybrid/run_qwen_32b_on_deepseek.sh
@@ -3,10 +3,10 @@ python hybrid_token.py --dataset gsm8k --thinking_model deepseek-ai/DeepSeek-R1-
 
 python hybrid_token.py --dataset math500 --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
 
-python hybrid_token.py --dataset aime24 --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
+python hybrid_token.py --dataset aime24 --only-finished-thinking --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
 
-python hybrid_token.py --dataset aime25 --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
+python hybrid_token.py --dataset aime25 --only-finished-thinking --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
 
-python hybrid_token.py --dataset mbpp --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
+python hybrid_token.py --dataset mbpp --only-finished-thinking --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
 
-python hybrid_token.py --dataset livecodebench --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
+python hybrid_token.py --dataset livecodebench --only-finished-thinking --thinking_model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --base_model Qwen/Qwen2.5-32B --steering_layer 24  --sae_layer 27 --n_clusters 15 --max_new_tokens 2000  --max_thinking_tokens 2000
